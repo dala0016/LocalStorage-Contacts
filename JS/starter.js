dd3ac34bff5f5fda9aadd0e88946e46ac228ffca
dalam3393 <32398041+dalam3393@users.noreply.github.com>
2017-12-22 18:17:10 -0500

    first change

---
+++ JS/starter.js
@@ -0,0 +1,17 @@
+let contactStarter = [
+{
+        "fullname": "Karan",
+        "phone": "613-123-4567",
+        "email": "dalam.333@gmail.com"
+    },
+    {
+        "fullname": "Geet",
+        "phone": "613-789-4567",
+        "email": "geet.pretty_girl@hotmail.com"
+    },
+    {
+        "fullname": "Jynx",
+        "phone": "613-858-8768",
+        "email": "jynx"
+    }
+]
